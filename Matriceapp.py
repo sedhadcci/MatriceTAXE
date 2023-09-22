94b33f636db32288d3ea18739285e28dd72bd6bd
sedhadcci <125959892+sedhadcci@users.noreply.github.com>
2023-09-22 14:45:19 +0200

    Update Matriceapp.py

--- Matriceapp.py
+++ Matriceapp.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import base64
+from io import BytesIO
+
+# Function to create downloadable excel link
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+def get_table_download_link(df, filename="matrice.xlsx", text="Télécharger la matrice Excel"):
+    val = to_excel(df)
+    b64 = base64.b64encode(val).decode()
+    return f'<a href="data:application/octet-stream;base64,{b64}" download="{filename}">{text}</a>'
 
 # Streamlit app setup
 st.title("Optimisation d'affectation de la taxe d'apprentissage")
@@ -12,13 +28,10 @@ if uploaded_file:
     # Read Excel file into a DataFrame, specifying dtype for SIRET columns
     df = pd.read_excel(uploaded_file, dtype={'SIRET ENTREPRISE': str, 'SIRET ETABLISSEMENT': str})
 
-    # Check for column existence
     if all(col in df.columns for col in ['SIRET ENTREPRISE', 'TA SOLDE PAIE', 'SIRET ETABLISSEMENT', 'MONTANT A ATTRIBUER']):
-        # Sort dataframes
         df_enterprises = df[['SIRET ENTREPRISE', 'TA SOLDE PAIE']].drop_duplicates().sort_values(by='TA SOLDE PAIE', ascending=False)
         df_schools = df[['SIRET ETABLISSEMENT', 'MONTANT A ATTRIBUER']].drop_duplicates().sort_values(by='MONTANT A ATTRIBUER', ascending=False)
 
-        # Create an empty DataFrame for the matrix, converting SIRET to str
         matrix_df = pd.DataFrame(index=['Montant total TA SOLDE PAIE', 'Reste à affecter'] + df_schools['SIRET ETABLISSEMENT'].astype(str).tolist(),
                                  columns=df_enterprises['SIRET ENTREPRISE'].astype(str).values)
         matrix_df.fillna(0, inplace=True)
@@ -37,18 +50,14 @@ if uploaded_file:
                 if remaining_amount_s == 0:
                     continue
 
-                # Calculate the amount to be attributed
                 attrib_amount = min(remaining_amount_e, remaining_amount_s)
 
-                # Update remaining amounts and the matrix
                 remaining_amount_e -= attrib_amount
                 df_schools.at[index_s, 'MONTANT A ATTRIBUER'] -= attrib_amount
                 matrix_df.at[row_s['SIRET ETABLISSEMENT'], row_e['SIRET ENTREPRISE']] = attrib_amount
 
-            # Fill the "Reste à affecter" row
             matrix_df.at['Reste à affecter', row_e['SIRET ENTREPRISE']] = remaining_amount_e
 
-        # Add percentage columns for each SIRET Entreprise
         for col in df_enterprises['SIRET ENTREPRISE'].astype(str):
             total_amount = matrix_df.loc['Montant total TA SOLDE PAIE', col]
             percentage_col_name = f"{col} (%)"
@@ -58,7 +67,6 @@ if uploaded_file:
             else:
                 matrix_df[percentage_col_name] = ((matrix_df[col] / total_amount) * 100).round(8)
 
-        # Reorder columns so that each SIRET Entreprise column is followed by its corresponding percentage column
         new_columns_order = []
         for col in df_enterprises['SIRET ENTREPRISE'].astype(str):
             new_columns_order.append(col)
@@ -68,6 +76,7 @@ if uploaded_file:
 
         st.write("Matrice d'affectation avec pourcentage :")
         st.write(matrix_df)
+        st.markdown(get_table_download_link(matrix_df), unsafe_allow_html=True)
 
     else:
         st.error("Le fichier Excel doit contenir les colonnes 'SIRET ENTREPRISE', 'TA SOLDE PAIE', 'SIRET ETABLISSEMENT', 'MONTANT A ATTRIBUER'.")
